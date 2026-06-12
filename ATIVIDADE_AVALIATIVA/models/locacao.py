0ea32c4ec8e969b678441e0e8a6aa9e087e92b45
BernardoAngelo <12400602@aluno.cotemig.com.br>
2026-06-12 11:03:38 -0300

    Add files via upload

---
+++ ATIVIDADE_AVALIATIVA/models/locacao.py
@@ -0,0 +1,24 @@
+from ...models import db
+from .base import ModeloBase
+
+# Dica: data_inicio/data_fim usam db.Date (importe Date se precisar)
+
+
+class Locacao(ModeloBase):
+    __tablename__ = "locacoes"
+
+    # TODO ALUNO: FK cliente_id → clientes_locadora.id
+    # TODO ALUNO: FK veiculo_id → veiculos.id
+    data_inicio = db.Column(db.Date, nullable=False)
+    data_fim = db.Column(db.Date, nullable=False)
+    valor_total = db.Column(db.Float,nullable=False)
+    id_cliente = db.Column(db.Integer, db.ForgeinKey, ('id_cliente'),nullable=False)
+    id_veiculo = db.Column(db.Integer, db.ForgeinKey, ('id_veiculo'),nullable=False)
+
+
+    # TODO ALUNO: relationship cliente e veiculo
+    veiculos = db.relationship('veiculos', back_populates='locacoes')
+
+    @classmethod
+    def listar_com_detalhes(cls):
+        return cls.query.order_by(cls.data_criacao.desc()).all()
---
+++ ATIVIDADE_AVALIATIVA/models/veiculo.py
@@ -0,0 +1,17 @@
+from ...models import db
+from .base import ModeloBase
+
+
+class Veiculo(ModeloBase):
+    __tablename__ = "veiculos"
+
+    placa = db.Column(db.String(10), nullable=False)
+    modelo = db.Column(db.String(80), nullable=False)
+    # TODO ALUNO: diaria (Float)
+    diaria = db.Column(db.Float, nullable=False)
+
+    # TODO ALUNO: relationship com Locacao
+
+    @classmethod
+    def listar(cls):
+        return cls.query.order_by(cls.modelo).all()
